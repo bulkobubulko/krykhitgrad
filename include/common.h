daa458f024f6c7d8bfce0890b665cbbaf19601c2
bulkobubulko <bulkobubulko@gmail.com>
2025-04-05 15:59:51 +0300

    Initial commit.

---
+++ include/common.h
@@ -0,0 +1,8 @@
+//
+// Created by Bulko Bubulko on 05.04.2025.
+//
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#endif //COMMON_H
---
+++ include/krykhitgrad.h
@@ -0,0 +1,8 @@
+//
+// Created by Bulko Bubulko on 05.04.2025.
+//
+
+#ifndef KRYKHITGRAD_H
+#define KRYKHITGRAD_H
+
+#endif //KRYKHITGRAD_H
